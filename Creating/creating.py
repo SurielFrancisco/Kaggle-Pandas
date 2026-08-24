1c81818c7e99fc110066a33b5d2375c699ab2901
Suriel Francisco <surielfrancisco@MacBook-Pro-de-Suriel.local>
2026-08-24 02:06:31 -0600

    Create_5

--- Creating/creating.py
+++ Creating/creating.py
@@ -1,6 +1,2 @@
-# Your code goes here. Create a dataframe matching the above diagram and assign it to the variable fruits.
-fruits = pd.DataFrame({"Apples": [30], "Bananas": [21]})
-
-# Check your answer
-q1.check()
-fruits
+animals = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=['Year 1', 'Year 2'])
+animals.to_csv("cows_and_goats.csv")
