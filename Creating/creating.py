f03db4dd02eab4061a9efae7f5f825c0512815ce
Suriel Francisco <surielfrancisco@MacBook-Pro-de-Suriel.local>
2026-08-24 02:03:19 -0600

    Create_1

---
+++ Creating/creating.py
@@ -0,0 +1,6 @@
+# Your code goes here. Create a dataframe matching the above diagram and assign it to the variable fruits.
+fruits = pd.DataFrame({"Apples": [30], "Bananas": [21]})
+
+# Check your answer
+q1.check()
+fruits
